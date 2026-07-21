5c896c45dd198dec061e7a78f10e46b54afdffb6
binra <79102117+binra@users.noreply.github.com>
2026-07-21 15:43:43 +0300

    update number Morecategorie

--- script.js
+++ script.js
@@ -429,7 +429,7 @@ async function loadMoreCategoriesMenu() {
 
             count++;
 
-            if (count > 5) {
+            if (count > 30) {
 
                 dynamicMoreCategories.innerHTML += `
 
